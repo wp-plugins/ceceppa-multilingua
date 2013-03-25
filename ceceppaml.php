1092ed17ba29c7542a11d748e7f702adc485d66f
ceceppa <ceceppa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-25 15:04:43 +0000

    0.5.3
    git-svn-id: https://plugins.svn.wordpress.org/ceceppa-multilingua/trunk@687031 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ceceppaml.php
+++ ceceppaml.php
@@ -3,7 +3,7 @@
 Plugin Name: Ceceppa Multilingua
 Plugin URI: http://www.ceceppa.eu/it/interessi/progetti/wp-progetti/ceceppa-multilingua-per-wordpress/
 Description: Come rendere il tuo sito wordpress multilingua :).How make your wordpress site multilanguage.
-Version: 0.5.2
+Version: 0.5.3
 Author: Alessandro Senese aka Ceceppa
 Author URI: http://www.ceceppa.eu/chi-sono
 License: GPL3
@@ -1755,7 +1755,7 @@ class CeceppaML {
 		$the_id = get_the_ID();
 		if(empty($the_id)) {
 			$url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-			$the_id  = url_to_postid($url);
+			//$the_id  = url_to_postid($url);
 		}
 
     //Categoria
