3998a2f91da4ac5d01c5a8b059a0189d93107eae
ceceppa <ceceppa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-02 13:20:51 +0000

    aggiornamento options.php 0.7.0
    git-svn-id: https://plugins.svn.wordpress.org/ceceppa-multilingua/trunk@690620 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- options.php
+++ options.php
@@ -26,7 +26,7 @@
 						<br />
 						<label for="url-mode-domain">
 							<input type="radio" name="url-mode" id="url-mode-domain" value="3" <?php checked(get_option("cml_modification_mode"), 3) ?> />
-							<?php _('Use Pre-Domain Mode', 'ceceppaml') ?><i>(en.example.com)</i>
+							<?php _e('Use Pre-Domain Mode', 'ceceppaml') ?><i>(en.example.com)</i>
 						</label>
 						<br /><br />
 						<input type="checkbox" id="add-slug" name="add-slug" value="1" <?php checked(get_option('cml_add_slug_to_link', true), true) ?> />
