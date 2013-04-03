ca1428661ea0cd2c7769edacb231fd6f281d4f79
ceceppa <ceceppa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-03 20:24:42 +0000

    delete 0.6.2
    git-svn-id: https://plugins.svn.wordpress.org/ceceppa-multilingua/trunk@691372 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 0.6.2/js/ceceppa.search.js
+++
@@ -1,8 +0,0 @@
-/* Modifico la voce action della classe formsearch */
-function isEmpty( inputStr ) { if ( null == inputStr || "" == inputStr ) { return true; } return false; }
-
-jQuery(document).ready(function() {
-  var input = jQuery('<input type="hidden" name="lang" value="' + cml_object.lang + '" />');
-  $form = jQuery('.' + cml_object.form_class);
-  $form.append(input);
-});
