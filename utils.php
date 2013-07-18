79ec51bb652402f55dbf178b5291f4d56c1303ac
ceceppa <ceceppa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-18 11:00:39 +0000

    Tag 1.2.4
    git-svn-id: https://plugins.svn.wordpress.org/ceceppa-multilingua/tags/1.2.4@742710 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- utils.php
+++ utils.php
@@ -42,10 +42,13 @@ function cml_get_linked_post($lang_id, $result, $post_id, $browser_lang = null) 
 
   $link = null;
 
-  if(empty($result)) $result = $wpdb->get_row(sprintf("SELECT * FROM %s WHERE id = %d", CECEPPA_ML_TABLE, $browser_lang));
+  if(empty($result)) :
+    if(empty($browser_lang)) $browser_lang = cml_get_current_language_id();
+    $result = $wpdb->get_row(sprintf("SELECT * FROM %s WHERE id = %d", CECEPPA_ML_TABLE, $browser_lang));
+  endif;
 
   //Non confronto la lingua con se stessa :D
-  if($result->id != $lang_id) {
+  if(is_object($result) && $result->id != $lang_id) {
       /*
 	* Devo cercare sia in cml_post_id_1 che in cml_post_id_2, xkè posso avere
 	* degli articoli collegati tra di loro, ma non a quella predefinita, e considerando
@@ -108,6 +111,186 @@ function isCrawler()
     }
     */
     return true;
+}
+
+// http://betterwp.net/wordpress-tips/url_to_postid-for-custom-post-types/
+function bwp_url_to_postid($url)
+{
+    global $wp_rewrite;
+ 
+    $url = apply_filters('url_to_postid', $url);
+ 
+    // First, check to see if there is a 'p=N' or 'page_id=N' to match against
+    if ( preg_match('#[?&](p|page_id|attachment_id)=(\d+)#', $url, $values) )   {
+        $id = absint($values[2]);
+        if ( $id )
+            return $id;
+    }
+ 
+    // Check to see if we are using rewrite rules
+    if(!is_object($wp_rewrite)) return;
+
+    $rewrite = $wp_rewrite->wp_rewrite_rules();
+
+    // Not using rewrite rules, and 'p=N' and 'page_id=N' methods failed, so we're out of options
+    if ( empty($rewrite) )
+	return 0;
+
+    // Get rid of the #anchor
+    $url_split = explode('#', $url);
+    $url = $url_split[0];
+ 
+    // Get rid of URL ?query=string
+    $url_split = explode('?', $url);
+    $url = $url_split[0];
+ 
+    // Add 'www.' if it is absent and should be there
+    if ( false !== strpos(home_url(), '://www.') && false === strpos($url, '://www.') )
+        $url = str_replace('://', '://www.', $url);
+ 
+    // Strip 'www.' if it is present and shouldn't be
+    if ( false === strpos(home_url(), '://www.') )
+        $url = str_replace('://www.', '://', $url);
+ 
+    // Strip 'index.php/' if we're not using path info permalinks
+    if (!$wp_rewrite->using_index_permalinks() )
+        $url = str_replace('index.php/', '', $url);
+ 
+    if ( false !== strpos($url, home_url()) ) {
+        // Chop off http://domain.com
+        $url = str_replace(home_url(), '', $url);
+    } else {
+        // Chop off /path/to/blog
+        $home_path = parse_url(home_url());
+        $home_path = isset( $home_path['path'] ) ? $home_path['path'] : '' ;
+        $url = str_replace($home_path, '', $url);
+    }
+ 
+    // Trim leading and lagging slashes
+    $url = trim($url, '/');
+ 
+    $request = $url;
+    // Look for matches.
+    $request_match = $request;
+    foreach ( (array)$rewrite as $match => $query) {
+        // If the requesting file is the anchor of the match, prepend it
+        // to the path info.
+        if ( !empty($url) && ($url != $request) && (strpos($match, $url) === 0) )
+            $request_match = $url . '/' . $request;
+ 
+        if ( preg_match("!^$match!", $request_match, $matches) ) {
+            // Got a match.
+            // Trim the query of everything up to the '?'.
+            $query = preg_replace("!^.+\?!", '', $query);
+ 
+            // Substitute the substring matches into the query.
+            $query = addslashes(WP_MatchesMapRegex::apply($query, $matches));
+ 
+            // Filter out non-public query vars
+            global $wp;
+            parse_str($query, $query_vars);
+            $query = array();
+            foreach ( (array) $query_vars as $key => $value ) {
+                if ( in_array($key, $wp->public_query_vars) )
+                    $query[$key] = $value;
+            }
+ 
+        // Taken from class-wp.php
+        foreach ( $GLOBALS['wp_post_types'] as $post_type => $t )
+            if ( $t->query_var )
+                $post_type_query_vars[$t->query_var] = $post_type;
+ 
+        foreach ( $wp->public_query_vars as $wpvar ) {
+            if ( isset( $wp->extra_query_vars[$wpvar] ) )
+                $query[$wpvar] = $wp->extra_query_vars[$wpvar];
+            elseif ( isset( $_POST[$wpvar] ) )
+                $query[$wpvar] = $_POST[$wpvar];
+            elseif ( isset( $_GET[$wpvar] ) )
+                $query[$wpvar] = $_GET[$wpvar];
+            elseif ( isset( $query_vars[$wpvar] ) )
+                $query[$wpvar] = $query_vars[$wpvar];
+ 
+            if ( !empty( $query[$wpvar] ) ) {
+                if ( ! is_array( $query[$wpvar] ) ) {
+                    $query[$wpvar] = (string) $query[$wpvar];
+                } else {
+                    foreach ( $query[$wpvar] as $vkey => $v ) {
+                        if ( !is_object( $v ) ) {
+                            $query[$wpvar][$vkey] = (string) $v;
+                        }
+                    }
+                }
+ 
+                if ( isset($post_type_query_vars[$wpvar] ) ) {
+                    $query['post_type'] = $post_type_query_vars[$wpvar];
+                    $query['name'] = $query[$wpvar];
+                }
+            }
+        }
+ 
+            // Do the query
+            $query = new WP_Query($query);
+            if ( !empty($query->posts) && $query->is_singular )
+                return $query->post->ID;
+            else
+                return 0;
+        }
+    }
+    return 0;
+}
+
+/**
+ * Retrieves a page given its path.
+ *
+ * @since 2.1.0
+ * @uses $wpdb
+ *
+ * @param string $page_path Page path
+ * @param string $output Optional. Output type. OBJECT, ARRAY_N, or ARRAY_A. Default OBJECT.
+ * @param array $post_type Optional. Post type. Default page.
+ * @return WP_Post|null WP_Post on success or null on failure
+ */
+function cml_get_page_by_path($page_path, $output = OBJECT, $post_type = array('page')) {
+	global $wpdb;
+
+    $page_path = rawurlencode(urldecode($page_path));
+    $page_path = str_replace('%2F', '/', $page_path);
+    $page_path = str_replace('%20', ' ', $page_path);
+    $parts = explode( '/', trim( $page_path, '/' ) );
+    $parts = array_map( 'esc_sql', $parts );
+    $parts = array_map( 'sanitize_title_for_query', $parts );
+
+    $in_string = "'". implode( "','", $parts ) . "'";
+    $post_type_sql = implode( "','", $post_type );
+//     $wpdb->escape_by_ref( $post_type_sql );
+    $pages = $wpdb->get_results("SELECT ID, post_name, post_parent, post_type FROM $wpdb->posts WHERE post_name IN ($in_string) AND (post_type IN ('$post_type_sql'))", OBJECT_K );
+    $revparts = array_reverse( $parts );
+
+    $foundid = 0;
+    foreach ( (array) $pages as $page ) {
+	    if ( $page->post_name == $revparts[0] ) {
+		    $count = 0;
+		    $p = $page;
+		    while ( $p->post_parent != 0 && isset( $pages[ $p->post_parent ] ) ) {
+			    $count++;
+			    $parent = $pages[ $p->post_parent ];
+			    if ( ! isset( $revparts[ $count ] ) || $parent->post_name != $revparts[ $count ] )
+				    break;
+			    $p = $parent;
+		    }
+
+		    if ( $p->post_parent == 0 && $count+1 == count( $revparts ) && $p->post_name == $revparts[ $count ] ) {
+			    $foundid = $page->ID;
+			    if ( $page->post_type == $post_type )
+				    break;
+		    }
+	    }
+    }
+
+    if ( $foundid )
+	    return get_post( $foundid, $output );
+
+    return null;
 }
 
 ?>
