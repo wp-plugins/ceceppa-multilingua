f35f6238e963adaf50482e9ee9d6d9c44a4b998b
ceceppa <ceceppa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-28 08:52:36 +0000

    Tag 1.2.8
    git-svn-id: https://plugins.svn.wordpress.org/ceceppa-multilingua/tags/1.2.8@747362 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/form_translations.php
+++ includes/form_translations.php
@@ -15,6 +15,13 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
+global $wpCeceppaML;
+
+//Non posso richiamare lo script direttamente dal browser :)
+if(!is_object($wpCeceppaML)) die("Access denied");
+
+global $wpdb;
 ?>
 
 <div class="wrap">
@@ -22,46 +29,49 @@
    <br />
     <form class="ceceppa-form" name="wrap" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>?page=ceceppaml-translations-page">
     <input type="hidden" name="form" value="1" />
-    <table class="ceceppaml CSSTableGenerator">
-      <tbody>
+    <table class="wp-list-table widefat wp-ceceppaml">
+      <thead>
       <tr>
-	  <td><img src="<?php echo cml_get_flag_by_lang_id(cml_get_default_language_id()) ?>" />&nbsp;<?php echo cml_get_language_title(cml_get_default_language_id()) ?></td>
+	  <th><img src="<?php echo cml_get_flag_by_lang_id(cml_get_default_language_id()) ?>" />&nbsp;<?php echo cml_get_language_title(cml_get_default_language_id()) ?></th>
 <?php
 	  $langs = cml_get_languages(1, 0);
 	  foreach($langs as $lang) {
 	    if(!$lang->cml_default) :
-	      echo "<td><img src='" . cml_get_flag($lang->cml_flag) . "'/>&nbsp;$lang->cml_language</td>";
+	      echo "<th><img src='" . cml_get_flag($lang->cml_flag) . "'/>&nbsp;$lang->cml_language</th>";
 	      $lid[] = $lang->id;
 	    endif;
 	  }
 ?>
-      <td style="width: 40px">
+      <th style="width: 40px">
 	<img src="<?php ECHO CECEPPA_PLUGIN_URL . "images/remove.png" ?>" height="16"/>
-      </td>
+      </th>
       </tr>
+      </thead>
 <?php 
   $results = $wpdb->get_results("SELECT min(id) as id, UNHEX(cml_text) as cml_text FROM " . CECEPPA_ML_TRANS . " WHERE cml_type='S' GROUP BY cml_text");
 
   $c = 0;
+  $size = 100 / (count($langs) + 1);
   foreach($results as $result) :
       $title = html_entity_decode($result->cml_text);
 	//Non posso utilizzare htmlentities perché sennò su un sito in lingua russa mi ritrovo tutti simboli strani :'(
-      $title = str_replace("\"", "&quot;", $title);
+      $title = str_replace("\"", "&quot;", stripslashes($title));
 
-      echo "<tr>";
+      $alternate = @empty($alternate) ? "alternate" : "";
+      echo "<tr class=\"${alternate}\">";
 
-      echo "<td style=\"height:2.5em\">\n";
+      echo "<td style=\"height:2.5em;width: $size%\">\n";
       echo "\t<input type=\"hidden\" name=\"id[]\" value=\"$result->id\" />\n";
       echo "\t<input type=\"hidden\" name=\"string[]\" value=\"$title\" />\n";
-      echo "$title</td>";
+      echo stripslashes($title) . "</td>";
       $i = 0;
 
       foreach($langs as $lang) :
-	$d = cml_translate($title, $lang->id);
-	$d = str_replace("\"", "&quot;", $d);
+	$d = cml_translate($title, $lang->id, 'S');
+	$d = str_replace("\"", "&quot;", stripslashes($d));
 	echo "<td>\n";
 	echo "<input type=\"hidden\" name=\"lang_id[$c][$i]\" value=\"$lang->id\" />\n";
-	echo "<input type=\"text\" name=\"value[$c][$i]\" value=\"$d\" /></td>\n";
+	echo "<input type=\"text\" name=\"value[$c][$i]\" value=\"$d\"  style=\"width: 100%\" /></td>\n";
 
 	$i++;
       endforeach;
@@ -80,8 +90,9 @@
      </tbody>
     </table>
     <div style="text-align:right">
-      <input type="button" class="ceceppa-salva" name="add" value="<?php _e('Add', 'ceceppaml') ?>" onclick="addRow(<?php echo count($langs) . ", '" . join(",", $lid) ?>')" />
-      <input type="submit" class="ceceppa-salva" name="action" value="<?php _e('Update', 'ceceppaml') ?>" />
+      <p class="submit">
+	<input type="button" class="button button-secondaty" name="add" value="<?php _e('Add', 'ceceppaml') ?>" onclick="addRow(<?php echo count($langs) . ", '" . join(",", $lid) ?>')" />	<?php submit_button( __('Update', 'ceceppaml', 'ceceppaml'), "button-primary", "action", false, 'class="button button-primary"' ); ?>
+      </p>
     </div>
     </form>
 </div>
