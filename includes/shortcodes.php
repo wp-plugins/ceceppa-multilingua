3d5c79916a4f0b6b2c2a9c6a7bfd1de2ac3fc831
ceceppa <ceceppa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-09 16:24:18 +0000

    Tag 1.4.17
    git-svn-id: https://plugins.svn.wordpress.org/ceceppa-multilingua/tags/1.4.17@890197 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -42,6 +42,11 @@ add_shortcode( 'cml_show_available_langs', 'cml_show_available_langs');
 add_shortcode( 'cml_other_langs_available', 'cml_shortcode_other_langs_available' );
 add_shortcode( 'cml_show_flags', 'cml_shortcode_show_flags' );
 add_shortcode( 'cml_translate', 'cml_shortcode_translate' );
+add_shortcode( 'cml_media_alt', 'cml_translate_media_alt' );
+
+foreach( CMLLanguage::get_all() as $lang ) {
+  add_shortcode( '_' . $lang->cml_language_slug . "_", 'cml_quick_shortcode' );
+}
 
 function cml_shortcode_text($attrs) {
   global $wpCeceppaML;
@@ -67,7 +72,7 @@ function cml_shortcode_translate( $attrs ) {
   return cml_translate( $string, $id );
 }
 
-function cml_do_shortcode($attrs) {
+function cml_do_shortcode( $attrs, $content = null ) {
   global $wpCeceppaML;
 
   $shortcode = $attrs['shortcode'];
@@ -75,7 +80,13 @@ function cml_do_shortcode($attrs) {
   
   $lang = @$attrs[ CMLLanguage::get_current_slug() ];
 
-  return do_shortcode("[$shortcode $params $lang]");
+  if( null == $content ) {
+    $do = "[$shortcode $params $lang]";
+  } else {
+    $do = "[$shortcode $params $lang]" . $content . "[/$shortcode]";
+  }
+  
+  return do_shortcode( $do );
 }
 
 function cml_show_available_langs( $args ) {
@@ -106,4 +117,33 @@ function cml_shortcode_show_flags($attrs) {
   return cml_show_flags( $attrs ); 
 }
 
+function cml_quick_shortcode( $attrs, $content = null, $shortcode ) {
+  if( preg_match( "/_(.*)_/", $shortcode, $match ) ) {
+    $lang = end( $match );
+
+    //not current one
+    if( ! CMLLanguage::is_current( $lang ) ) return "";
+
+    return do_shortcode( $content );
+  }
+
+  return $content;
+}
+
+function cml_translate_media_alt( $attrs, $content ) {
+  $lang = CMLLanguage::get_current_id();
+  
+  if( CMLLanguage::is_default( $lang ) ) return $content;
+
+  $id = $attrs[ 'id' ];
+
+  $meta = get_post_meta( $id, '_cml_media_meta', true );
+  if( isset( $meta[ 'alternative-' . $lang ] ) &&
+      ! empty( $meta[ 'alternative-' . $lang ] ) ) {
+    
+    return $meta[ 'alternative-' . $lang ];
+  }
+  
+  return $content;
+}
 ?>
