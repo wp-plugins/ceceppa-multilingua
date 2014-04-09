b056e96359fb3c1204871abee456f4f1065e5b78
ceceppa <ceceppa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-09 20:34:37 +0000

    Tag 1.4.18
    git-svn-id: https://plugins.svn.wordpress.org/ceceppa-multilingua/tags/1.4.18@890276 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/layouts/translate-my.php
+++ admin/layouts/translate-my.php
@@ -53,9 +53,9 @@ EOT;
       <?php
         $table = new MyTranslations_Table( $types );
         $table->prepare_items();
-      
+
         $table->display();
-        
+
         $lkeys = array_keys( CMLLanguage::get_all() );
       ?>
       <div style="text-align:right">
@@ -72,6 +72,9 @@ EOT;
   function cml_admin_update_my_translations() {
     global $wpdb;
 
+    //Check for what language I have to hide translation field for default language
+    $hide_for = apply_filters( "cml_my_translations_hide_default", array( 'S' ) );
+
     CMLTranslations::delete( "N" );
     CMLTranslations::delete( "S" );
 
@@ -87,8 +90,8 @@ EOT;
       foreach( $langs as $lang ) {
         $value = esc_attr( $_POST[ 'values' ][ $lang->id ][ $i ] );
 
-        if( $group == "S" &&
-           $lang->id == CMLLanguage::get_default_id() ) {
+        if( $lang->id == CMLLanguage::get_default_id() &&
+            in_array( $group, $hide_for ) ) {
           continue;
         }
 
@@ -114,6 +117,6 @@ EOT;
     }
 
     //generate .po
-    cml_generate_mo_from_translations( "S", true );
+    cml_generate_mo_from_translations( "_X_", true );
   }
 ?>
